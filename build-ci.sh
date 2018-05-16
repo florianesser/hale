64675a86c2d3bf11611557e130bfce34a5eee50f
Florian Esser <fe@wetransform.to>
2018-05-16 15:08:35 +0200

    Plain console for Gradle

--- build-ci.sh
+++ build-ci.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./build/gradlew -p build -Dorg.gradle.jvmargs="-XX:MaxPermSize=512M" -Dorg.gradle.daemon=false --stacktrace cli -Pargs="$*"
+./build/gradlew -p build -Dorg.gradle.jvmargs="-XX:MaxPermSize=512M" -Dorg.gradle.daemon=false --console plain --stacktrace cli -Pargs="$*"
